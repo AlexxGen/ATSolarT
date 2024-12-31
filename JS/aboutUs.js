064951013c509be705ca36bc47366f319a2fd54b
AlexxGen <alexx.genchev@gmail.com>
2024-12-30 20:30:49 -0700

    updated about us page

--- JS/aboutUs.js
+++ JS/aboutUs.js
@@ -16,3 +16,13 @@ window.addEventListener("scroll", function(){
     }    
 })
 
+// Change image on mouse hover
+$(".imgStack").mouseover(function(){
+    $(this).find(".layer0").addClass("transparent");
+    $(this).find(".layer1").removeClass("transparent");
+});
+
+$(".imgStack").mouseout(function(){
+    $(this).find(".layer0").removeClass("transparent");
+    $(this).find(".layer1").addClass("transparent");
+});
